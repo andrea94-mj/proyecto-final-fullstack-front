6e61e91ed84e9049abe7188085d7f0508c86555c
Andrea Miguel Jarillo <andreamj94@gmail.com>
2025-04-27 23:00:46 +0200

    commit

--- front/src/pages/Admin.jsx
+++ front/src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '@/css/Admin.css'; 
+import "@/css/Admin.css"; 
 
 const Admin = () => {
   // Estados para almacenar los datos
